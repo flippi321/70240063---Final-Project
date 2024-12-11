6f7d714dbc26adc8ef1fda768cd3f2558e68cccf
flippi321 <90700334+flippi321@users.noreply.github.com>
2024-12-11 20:35:38 +0800

    Made main.py start docker compose

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import os
+import subprocess
 from utils.data_generation import generate_data
 from utils.data_partitioning import partition_all
 
+should_compose = False
 should_generate = False
+should_partition = False
+
+if(should_compose):
+    # Compose the docker containers
+    try:
+        print("Composing the docker containers...")
+        subprocess.run(["docker-compose", "up", "-d"], check=True)
+        print("Docker containers composed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error during docker compose: {e}")
 
 if should_generate:
     # We generate 10GB of data
@@ -18,14 +30,24 @@ if should_generate:
 else:
     generated = True
 
-# We partition the data
-print("Partitioning data...")
-partitioned = partition_all(
-    input_dir="data/database/dat_files", 
-    output_dir="data/database/partitioned")
+if should_partition:
+    # We partition the data
+    print("Partitioning data...")
+    partitioned = partition_all(
+        input_dir="data/database/dat_files", 
+        output_dir="data/database/partitioned")
+else:
+    partitioned = True
+
+# Run mongoimport commands after partitioning the data
+if partitioned:
+    print("Importing data into MongoDB...")
+    uploaded_data = True
+else:
+    print("Data partitioning failed")
 
 
-if (generated and partitioned):
-    print("Data generation and partitioning completed sucessfully")
+if (generated and partitioned and uploaded_data):
+    print("Data generation and partitioning completed successfully")
 else:
     print("Data generation failed" if not generated else "Data partitioning failed")
